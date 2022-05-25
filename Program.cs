28641e581ecbbde2a6a977f7fce92b1f202c4650
CamilyMoreira <camily.camargo2004@gmail.com>
2022-05-25 10:49:58 -0300

    isolamos a classe configuração do banco de dados (Data Source=database.db) em uma classe separada

--- Program.cs
+++ Program.cs
@@ -3,9 +3,11 @@ using Microsoft.Data.Sqlite;
 using LabManager.Database;
 using LabManager.Repositories;
 
-var databaseSetup = new DatabaseSetup();
+var databaseConfig = new DatabaseConfig(); //objeto; n precisa ficar criando nas classes 
 
-var computerRepository = new ComputerRepository();
+var databaseSetup = new DatabaseSetup(databaseConfig);
+
+var computerRepository = new ComputerRepository(databaseConfig);
 
 //Routing
 var modelName = args[0];
@@ -29,7 +31,7 @@ if(modelName == "Computer")
         var processor = args[4];
 
         //Connection
-        var connection = new SqliteConnection("Data Source=database.db");
+        var connection = new SqliteConnection(databaseConfig.ConnectionString);
         connection.Open();
 
         //Command
